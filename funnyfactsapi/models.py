344ff41edf739298c04d96d7f4e0919d17fcf2a7
mpasz <mpasz@wpc.com.pl>
2022-06-11 16:23:14 +0200

    initial migrations

--- funnyfactsapi/models.py
+++ funnyfactsapi/models.py
@@ -2,9 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class CallendarMonths(models.Model):
-    month_name = models.CharField(max_length=15, null=False)
-
 class FunnyFacts(models.Model):
     day = models.SmallIntegerField(null=False)
     month = models.SmallIntegerField(null=False)
