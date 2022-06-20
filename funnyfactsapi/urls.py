b4e689a027fe752cf67beef058cb5e6cfa4c74f0
mpasz <mpasz@wpc.com.pl>
2022-06-20 09:07:35 +0200

    urlpatters modifications

--- funnyfactsapi/urls.py
+++ funnyfactsapi/urls.py
@@ -1,13 +1,24 @@
-from rest_framework import routers
-from . import views
-
-router = routers.DefaultRouter()
-router.register('dates', views.FunnyFactsViewSet, basename="list")
-router.register('popular', views.PopularFunnyFactViewSet, basename="popular-facts-list")
-
-
-urlpatterns = (
-    router.urls
+from django.urls import path
+from funnyfactsapi.views import (
+    funny_fact_list,
+    funny_fact_detail
 )
+
+app_name = 'funny_fact'
+
+urlpatterns = [
+    path('dates/', funny_fact_list, name='list'),
+    path('dates/<int:id>/', funny_fact_detail, name='detail'),
+]
+
+
+# router = routers.DefaultRouter()
+# router.register('dates', views.FunnyFactsViewSet, basename="list")
+# router.register('popular', views.PopularFunnyFactViewSet, basename="popular-facts-list")
+
+
+# urlpatterns = (
+#     router.urls
+# )
 
 
